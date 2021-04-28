23f96c461685d95013728e53f63813ac5734a316
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-04-28 16:16:47 -0500

    create themes

---
+++ MyEnvironment/AppTheme.swift
@@ -0,0 +1,43 @@
+//
+//  AppTheme.swift
+//  MyEnvironment
+//
+//  Created by Desiree on 4/28/21.
+//
+
+
+import SwiftUI
+
+protocol AppTheme {
+    var primaryFont: Font { get set }
+    var secondaryFont: Font { get set }
+    var mainColor: Color { get set }
+    var accentColor: Color { get set }
+}
+
+class Theme1: AppTheme {
+    var accentColor: Color = Color.init(.lightGray)
+    var primaryFont: Font = Font.custom("NotoSansMyanmar-Bold", size: 26)
+    var secondaryFont: Font = Font.custom("NotoSansMyanmar-Light", size: 18)
+    var mainColor: Color = Color.init(.darkGray)
+}
+
+class Theme2: AppTheme {
+    var accentColor: Color = Color.init(.purple)
+    var primaryFont: Font = Font.custom("PingFangHK-Medium", size: 26)
+    var secondaryFont: Font = Font.custom("PingFangHK-UltraLight", size: 18)
+    var mainColor: Color = Color.init(.blue)
+}
+
+fileprivate let availableThemes:[AppTheme] = [Theme1(), Theme2()]
+class SelectedTheme: ObservableObject {
+    @Published var theme: AppTheme = availableThemes[0]
+    
+    func switchThemes() {
+        if let _ = theme as? Theme1 {
+            theme = availableThemes[1]
+        } else {
+            theme = availableThemes[2]
+        }
+    }
+}
