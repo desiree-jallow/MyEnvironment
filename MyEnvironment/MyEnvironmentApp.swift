e44deeda2cbc3bf4827f4f09d9a5fc9383bb3ba2
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-04-28 14:59:21 -0500

    Initial Commit

---
+++ MyEnvironment/MyEnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyEnvironmentApp.swift
+//  MyEnvironment
+//
+//  Created by Desiree on 4/28/21.
+//
+
+import SwiftUI
+
+@main
+struct MyEnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
